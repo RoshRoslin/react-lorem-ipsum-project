625cf8798ed1092d2a1870b494d35456573fcac9
RoshRoslin <RoshRoslin@gmail.com>
2021-05-09 14:08:54 -0700

    Updates

---
+++ precache-manifest.01c27ec5cd4fe3a1c2a12ebfc1b4e02e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "783c6d7257b219474a3bc1c467e69e2f",
+    "url": "/react-lorem-ipsum-project/index.html"
+  },
+  {
+    "revision": "7157f9d6994ab82dce04",
+    "url": "/react-lorem-ipsum-project/static/css/main.fb86bce3.chunk.css"
+  },
+  {
+    "revision": "d1ccc1e6d556dbe2d9b7",
+    "url": "/react-lorem-ipsum-project/static/js/2.d8a71053.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-lorem-ipsum-project/static/js/2.d8a71053.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7157f9d6994ab82dce04",
+    "url": "/react-lorem-ipsum-project/static/js/main.d59e2259.chunk.js"
+  },
+  {
+    "revision": "348a003e2328dee2cbe8",
+    "url": "/react-lorem-ipsum-project/static/js/runtime-main.196c0f9c.js"
+  }
+]);
